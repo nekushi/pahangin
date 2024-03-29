519e7710ab4ffbc5fae6aa0b219ca9fc1f0bd386
nekushi <122607874+nekushi@users.noreply.github.com>
2024-03-29 20:52:32 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const firstBlade = document.querySelector(".blade:nth-child(1)")
 const secondBlade = document.querySelector(".blade:nth-child(2)")
 const thirdBlade = document.querySelector(".blade:nth-child(3)")
+const allBlade = document.querySelectorAll(".blade")
 
 // const btn0 = document.querySelector(".btn0")
 // const btn1 = document.querySelector(".btn1")
 // const btn2 = document.querySelector(".btn2")
 // const btn3 = document.querySelector(".btn3")
 
-btn0 = switchOn(0, 50)
-btn1 = switchOn(10, 50)
-btn2 = switchOn(20, 50)
-btn3 = switchOn(30, 50)
+btn0 = switchOn(0)
+btn1 = switchOn(1000)
+btn2 = switchOn(500)
+btn3 = switchOn(250)
 
 let baseFirstBlade = 0
 let baseSecondBlade = 120
@@ -19,13 +20,19 @@ let baseThirdBlade = 240
 function switchOn(power) {
     return function() {
         if (power !== 0) {
-            forever = setInterval(() => {
-                firstBlade.style.transform = `rotate(${baseFirstBlade += power}deg)`
-                secondBlade.style.transform = `rotate(${baseSecondBlade += power}deg)`
-                thirdBlade.style.transform = `rotate(${baseThirdBlade += power}deg)`
-            }, 150)
+            for (let i = 0; i < allBlade.length; ++i) {
+                if (i === 0) {
+                    allBlade[i].style.animation = `rotate1 ${power}ms linear infinite forwards`
+                } else if (i === 1) {
+                    allBlade[i].style.animation = `rotate2 ${power}ms linear infinite forwards`
+                } else if (i === 2) {
+                    allBlade[i].style.animation = `rotate3 ${power}ms linear infinite forwards`
+                }
+            }
         } else {
-            location.reload()
+            allBlade.forEach(blade => {
+                blade.style.animation = "none"
+            })
         }
     }
 }
